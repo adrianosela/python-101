7101c25385247cc3849e56a6fcb0eab5fad28747
Adriano <adriano.selaviles@gmail.com>
2020-04-15 12:26:33 -0700

    add first exercise

--- lab-0/README.md
+++ lab-0/README.md
@@ -1,3 +1,22 @@
 ### Lab 0 - Lessons 0 to 3
 
-// TODO 
+This lab will keep things simple. We will do 5 small exercises, incremental in difficulty.
+
+Review your mathematical operators, literal types, lists, conditions,
+loops, and be sure to comment out your code where you deem necessary.
+
+Exercises are self contained in the files:
+
+* `a.py`
+* `b.py` (TBD)
+* `c.py` (TBD)
+* `d.py` (TBD)
+* `e.py` (TBD)
+
+The solutions are in the corresponding files:
+
+* `a_soln.py`
+* `b_soln.py` (TBD)
+* `c_soln.py` (TBD)
+* `d_soln.py` (TBD)
+* `e_soln.py` (TBD)
---
+++ lab-0/a.py
@@ -0,0 +1,19 @@
+"""
+You are given a list representing the amount of water
+required by some plants. Your water container can hold
+only 1000mL (1L) at a time. Your task is to find the
+number of times you will need to fill your container in
+order to water all the plants. 
+
+Assume that you can NOT begin watering a plant unless you
+have all of the water you need in your container. That
+is, if a plant requires 800mL of water, and you only 
+have 400mL in your container, you MUST refill immediately.
+i.e. You can't pour half now, and half after you've refilled.
+"""
+
+containerMax = 800
+plants = [120, 250, 250, 150, 420, 250, 300, 750, 180, 450]
+
+# TODO: write your code here.
+# hint - you will need a loop, and some variables
