1b635abbec9ec333244d154e81df7cf0c5052717
Adriano <adriano.selaviles@gmail.com>
2020-04-14 19:30:00 -0700

    fixed while.py

--- lesson-3/while.py
+++ lesson-3/while.py
@@ -1,11 +1,16 @@
-numbers = [1, 3, 9, 6, 0, 4, 2, 6, 5, 8, 5, 9, 2, 7, 4, 0, 1]
+numbers = [1, 3, 9, 6, 0, 4, 2, 6, 5, 8, 9, 2, 5, 7, 4, 0, 1]
 
 found = False
 index = -1
 
-while(not found):
+while not found:
     index = index + 1
+    if index > len(numbers) - 1:
+        break
     if numbers[index] == 5:
         found = True
-    
-print("first occurence of 5 happens at index", index)
+
+if found:
+    print("first occurence of 5 happens at index", index)
+else:
+    print("no instances of number 5 in list")
