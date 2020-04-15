5797dac3cdea89e4284d472b4d8213893fb61cdb
Adriano <adriano.selaviles@gmail.com>
2020-04-15 12:28:47 -0700

    update exercise

--- lab-0/a_soln.py
+++ lab-0/a_soln.py
@@ -3,11 +3,12 @@ You are given a list representing the amount of water
 required by some plants. Your water container can hold
 only 1000mL (1L) at a time. Your task is to find the
 number of times you will need to fill your container in
-order to water all the plants. 
+order to water all the plants. Your container starts
+empty, i.e. with 0 mL.
 
 Assume that you can NOT begin watering a plant unless you
 have all of the water you need in your container. That
-is, if a plant requires 800mL of water, and you only 
+is, if a plant requires 800mL of water, and you only
 have 400mL in your container, you MUST refill immediately.
 i.e. You can't pour half now, and half after you've refilled.
 """
