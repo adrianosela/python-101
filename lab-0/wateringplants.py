72e4a242fb0928eebce471e0de96e5c7dbf79a48
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2020-04-16 11:05:48 -0700

    Update wateringplants.py

--- lab-0/wateringplants.py
+++ lab-0/wateringplants.py
@@ -1,10 +1,11 @@
 """
 You are given a list representing the amount of water
 required by some plants. Your water container can hold
-only 1000mL (1L) at a time. Your task is to find the
-number of times you will need to fill your container in
-order to water all the plants. Your container starts
-empty, i.e. with 0 mL.
+only 1000mL (1L) at a time. 
+
+Your task is to find the number of times you will need
+to fill your container in order to water all the plants.
+Your container starts empty, i.e. with 0 mL.
 
 Assume that you can NOT begin watering a plant unless you
 have all of the water you need in your container. That
