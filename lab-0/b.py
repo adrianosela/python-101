f605cb6031648371d3f3ca191ce1bd5730abf751
Adriano <adriano.selaviles@gmail.com>
2020-04-16 10:02:06 -0700

    add suggestion for b

--- lab-0/b.py
+++ lab-0/b.py
@@ -2,7 +2,10 @@
 Your task is to print the (x, y) coordinates of the single 'o' character
 in a grid, represented as a list of lists.
 
-Hint - you can nest loops within loops
+Hint - you can nest loops within loops as follows:
+for <var_x> in <iterator_x>:
+    for <var_y> in <interator_y>:
+        <statements>
 """
 
 grid = [
